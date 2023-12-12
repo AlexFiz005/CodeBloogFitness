96b62aa1a63ebfe4d206fe1a465d8c3c2963bb8c
Alex Fiz <alexf.webdev@gmail.com>
2023-12-12 12:24:17 +0300

    Добавлено модульное тестирование для контроллера пользователя.

---
+++ CodeBlogFitness.BLTests/Controllers/UserControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeBlogFitness.BL.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Controllers.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            // Arrange
+            var userName = Guid.NewGuid().ToString();
+
+            // Act
+            var controller = new UserController(userName);
+
+            // Assert
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+        }
+    }
+}
